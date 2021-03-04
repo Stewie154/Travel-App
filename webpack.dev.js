c188d71ca84859c989fdb0cfef6be18639d32e26
Stewart <stewart@toru.digital>
2021-03-04 15:21:16 +0000

    End of Basics of Webpack
    After lots of installing / uninstalling, the html is now rendering in the webpack dev server. Next I'll move on to compiling the styles.

---
+++ webpack.dev.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/client/index.js',
+    stats: 'verbose',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+}
